6ec9aaff9880318e02704cffa5951e109aea8e30
Sergio Barreras <sergiobg.isc@gmail.com>
2024-04-03 09:25:21 -0700

    Implementar la funcionalidad de Guards

--- src/app/auth/guards/can-activate.guard.ts
+++ src/app/auth/guards/can-activate.guard.ts
@@ -1,14 +1,23 @@
+import { inject } from '@angular/core';
 import {
   ActivatedRouteSnapshot,
   CanActivateFn,
+  Router,
   RouterStateSnapshot,
 } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { Observable, tap } from 'rxjs';
 
 export const canActivateGuard: CanActivateFn = (
   route: ActivatedRouteSnapshot,
   state: RouterStateSnapshot
-) => {
-  console.log('activated');
-  console.log({route, state});
-  return true;
+): boolean | Observable<boolean> => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  return authService.checkAuthentication().pipe(
+    tap((isAuthenticated) => {
+      if (!isAuthenticated) router.navigate(['./auth/login']);
+    })
+  );
 };
--- src/app/auth/guards/can-match.guard.ts
+++ src/app/auth/guards/can-match.guard.ts
@@ -1,10 +1,18 @@
-import { CanMatchFn, Route, UrlSegment } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanMatchFn, Route, Router, UrlSegment } from '@angular/router';
+import { Observable, tap } from 'rxjs';
+import { AuthService } from '../services/auth.service';
 
 export const canMatchGuard: CanMatchFn = (
   route: Route,
   segments: UrlSegment[]
-) => {
-  console.log('match');
-  console.log({ route, segments });
-  return true;
+): boolean | Observable<boolean> => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  return authService.checkAuthentication().pipe(
+    tap((isAuthenticated) => {
+      if (!isAuthenticated) router.navigate(['./auth/login']);
+    })
+  );
 };
