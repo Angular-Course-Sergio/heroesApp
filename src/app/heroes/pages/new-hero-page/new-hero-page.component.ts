9032c15b34cf7fc82667ae2bb7d46dd8c00cc08c
Sergio Barreras <sergiobg.isc@gmail.com>
2024-04-03 00:09:07 -0700

    HeroForm - ReactiveForms introducción

--- src/app/heroes/pages/new-hero-page/new-hero-page.component.ts
+++ src/app/heroes/pages/new-hero-page/new-hero-page.component.ts
@@ -1,3 +1,4 @@
+import { FormControl, FormGroup } from '@angular/forms';
 import { Component } from '@angular/core';
 import { Publisher } from '../../interfaces/hero.interface';
 
@@ -7,5 +8,15 @@ import { Publisher } from '../../interfaces/hero.interface';
   styleUrl: './new-hero-page.component.css',
 })
 export class NewHeroPageComponent {
+  public heroForm = new FormGroup({
+    id: new FormControl<string>(''),
+    superhero: new FormControl<string>('', { nonNullable: true }),
+    publisher: new FormControl<Publisher>(Publisher.DCComics),
+    alter_ego: new FormControl(''),
+    first_appearance: new FormControl(''),
+    characters: new FormControl(''),
+    alt_img: new FormControl(''),
+  });
+
   public publishers = Object.values(Publisher);
 }
