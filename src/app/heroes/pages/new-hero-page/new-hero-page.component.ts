4e5a93e27f39e1dad4fc4cdbddfe76b768469636
Sergio Barreras <sergiobg.isc@gmail.com>
2024-04-02 22:41:58 -0700

    Agregar héroe

--- src/app/heroes/pages/new-hero-page/new-hero-page.component.ts
+++ src/app/heroes/pages/new-hero-page/new-hero-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { Publisher } from '../../interfaces/hero.interface';
 
 @Component({
   selector: 'app-new-hero-page',
   templateUrl: './new-hero-page.component.html',
-  styleUrl: './new-hero-page.component.css'
+  styleUrl: './new-hero-page.component.css',
 })
 export class NewHeroPageComponent {
-
+  public publishers = Object.values(Publisher);
 }
